c6e48045a75cabeb737454127158c6f37c2cc209
MelvinGGYY <32861801+MelvinGGYY@users.noreply.github.com>
2019-04-02 16:28:27 +0800

    Update Java_Object_oriented.java

--- Java_Object_oriented.java
+++ Java_Object_oriented.java
@@ -3,17 +3,17 @@ import Java.util.Scanner;
 public class ClassWork{
 	public static void main(String[] args){
 
-		Scanner input=new Scanner(System.in);
+	Scanner input=new Scanner(System.in);
 
-		//Chapter 1 page11
-		//第一题：Java执行过程：编写->编译->执行，编译原理：虚拟机编译
-		//第二题：Plan table
-		System.out.println("1.每天听课2小时，边学边练习，练习至少4个小时");
-		System.out.println("2.学习时间：早上八点到十一点，晚上：七点到十点");
-		System.out.println("3.本周学完java基础语法课程的专题1和专题2，阅读完教材，完成所有练习加油！\n");
+	//Chapter 1 page11
+	//第一题：Java执行过程：编写->编译->执行，编译原理：虚拟机编译
+	//第二题：Plan table
+	System.out.println("1.每天听课2小时，边学边练习，练习至少4个小时");
+	System.out.println("2.学习时间：早上八点到十一点，晚上：七点到十点");
+	System.out.println("3.本周学完java基础语法课程的专题1和专题2，阅读完教材，完成所有练习加油！\n");
 
-		//第三题：schedule
-		System.out.println("\&\&\&\&\&\&*课程表*");
+	//第三题：schedule
+	System.out.println("\&\&\&\&\&\&*课程表*");
         System.out.println("--------------------------------");
         System.out.println("星期 \&\&早上 \&\&下午");
         System.out.println("--------------------------------");
@@ -57,7 +57,97 @@ public class ClassWork{
         System.out.println("存款三年后的本息是："+(money*(3+0.0324)));
         System.out.println("存款五年后的本息是："+(money*(5+0.036)));
 
+        //Chapter 3 page60
+        //第一题：输入数字，输出最大值和最小值，输入0结束
+        int max;
+        int min;
+        int num;
+        System.out.print("请输入一个整数：");
+        min=max=input.nextInt();
+        System.out.print("请输入一个整数：");
+        num = input.nextInt();
+        if (num>max){
+                max = num;
+        }else {
+                min = num;
+        }
+        while(true){
+               System.out.print("请输入一个整数（输入0结束）：");
+               num= input.nextInt();            
+               if(num==0){
+                break;
+               }
 
+                if (num>max){
+                max = num;
+                }else if(num<min){
+                min = num;
+                }
+        }
+        System.out.print("最大值是："+max+"，最小值是："+min);
 
-	}
+        //第二题：输入1-7，输出对应星期，输入0程序结束
+       int d;
+        do{
+            System.out.println("请输入数字1-7，输入0结束：");
+             d = input.nextInt();
+             System.out.print("今天是 ");
+                        switch(d){
+                                case 1:
+                                System.out.println("Mon");
+                                break;
+                                case 2:
+                                System.out.println("Tues");
+                                break;
+                                case 3:
+                                System.out.println("Wed");
+                                break;
+                                case 4:
+                                System.out.println("Turs");
+                                break;
+                                case 5:
+                                System.out.println("Fir");
+                                break;
+                                case 6:
+                                System.out.println("Sat");
+                                break;
+                                case 7:
+                                System.out.println("Sun");
+                                break;
+                                default:
+                                System.out.println("wrong day");
+                                break;
+                        }
+                        
+                 }while(d!=0);
+
+        //第三题：机票原价5000，4~10月旺季，11~3月淡季，旺季头等舱9折，经济城6折，淡季头等舱5折，经济舱4折
+
+        System.out.print("请输入您出行的月份1~12:");
+        int tMonth = input.nextInt();
+        System.out.print("请选择舱位:（头1，经2）：");
+        int pClass= input.nextInt();
+        if(tMonth>=4&&tMonth<=10){
+                switch(pClass){
+                  case 1:
+                  System.out.println("4500rmb");
+                  break;
+                  case 2:
+                  System.out.println("3000rmb");
+                  break;
+                  default:
+                  break;
+                }
+        }else{
+               switch(pClass){
+                  case 1:
+                  System.out.println("2500rmb");
+                  break;
+                  case 2:
+                  System.out.println("2000rmb");
+                  break;
+                  default:
+                  break;  
+        }
+}
 }
